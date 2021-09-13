1c681d14108285f6bb5e5564c40629fad9447e6c
prod.astri <58519454+prod-astri@users.noreply.github.com>
2021-09-13 11:26:26 +0200

    modify Event model to include start and end

--- models/Event.js
+++ models/Event.js
@@ -3,15 +3,17 @@ const Schema = mongoose.Schema;
 const eventSchema = new Schema({
     title: String,
     description: String,
+    location: String,
     creator: {
         // to connect a schema from  an other document
         type: Schema.Types.ObjectId,
         // so this is the part connecting to the authors collection
         ref: 'User.model'
     },
-    location: String,
-    startTime: Date,
-    endTime: Date
+    timeAndDate {
+        startTime: Date,
+        endTime: Date,
+    }
 });
 
 const Event = mongoose.model('Event', eventSchema);
