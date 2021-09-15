998b76c55c6030c24563095613f30b446efc5e0c
HannesCho <63042736+HannesCho@users.noreply.github.com>
2021-09-15 11:32:56 +0200

    search done

--- routes/search.js
+++ routes/search.js
@@ -3,12 +3,22 @@ const Event = require('../models/Event');
 require("dotenv/config");
 
 router.post('/search', (req, res, next) => {
-    const searchTerm = req.body.search;
-    const regex = { '$regex': searchTerm, '$options': 'i' }
-    Event.find({ $or: [{ title: regex } , { description: regex}, { city: regex } ]})
+    const searchTerm = req.body.search.toLowerCase();
+    console.log(searchTerm);
+    Event.find()
         .then(eventsFromDB => {
-            console.log(eventsFromDB);
-            res.render("search", { eventList: eventsFromDB, word: searchTerm });
+            let eventList = eventsFromDB.filter(el => {
+                // field for search is in keys
+                const keys = [ el['title'], el['description'], el['location'], el['sports'], el['address']['street'], el['address']['city'], el['address']['country'] ];
+                for (let key of keys) {
+                    if (key) {
+                        if (key.toLowerCase().includes(searchTerm)) {
+                            return true
+                        }
+                    }
+                }
+            })
+            res.render("search", { eventList: eventList, word: searchTerm });
         })
         .catch(err => next(err));
  
