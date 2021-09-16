c4896e686b4888a72b45a64bc1ec6cc09f67f492
prod.astri <58519454+prod-astri@users.noreply.github.com>
2021-09-16 14:13:25 +0200

    fix profile page event list; change font

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,12 @@ router.get('/profile', loginCheck(), (req, res, next) => {
   const loggedInUser = req.user;
   Event.find({creator: loggedInUser._id}).sort({'timeAndDate.starting': -1})
   .then(eventsFromDB => {
+    for (ev of eventsFromDB){
+      const starting = ev.timeAndDate.starting.toLocaleString();
+      const ending = ev.timeAndDate.ending.toLocaleString();
+      ev.starting = starting;
+      ev.ending = ending;
+    }
     res.render('user/profile', { user: loggedInUser, eventList: eventsFromDB });
   })
   .catch(err => {
