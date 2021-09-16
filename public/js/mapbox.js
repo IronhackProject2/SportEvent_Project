b367024861985917dc9f6d40ad101158342ce60c
HannesCho <63042736+HannesCho@users.noreply.github.com>
2021-09-16 17:38:27 +0200

    update detail page map

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -3,22 +3,22 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiaGFubmVzY2hvIiwiYSI6ImNrdGxjN3FnYzAwYWMyb3Iwa
 const center = [document.querySelector('#hiddenLon').innerHTML, document.querySelector('#hiddenLat').innerHTML]; //[13.4532321, 52.5331092]
 const coords = JSON.parse(document.querySelector('#hiddenPositions').innerHTML);
 
-// set title and href for popups
-const titles = [];
-const hrefs = [];
-document.querySelectorAll(".title").forEach(el=> titles.push(el.outerText));
-document.querySelectorAll(".title").forEach(el=> hrefs.push(el.href.slice(el.href.indexOf('events')-1)));
-const title = titles[0];
-const href = hrefs[0];
-
 const map = new mapboxgl.Map({
-	
 	container: 'map', // container ID
 	style: 'mapbox://styles/mapbox/streets-v11', // style URL
 	center: center, // starting position [lng, lat]
 	zoom: 9 // starting zoom
 });
 
+if (document.querySelector(".title")) {
+	// set title and href for popups
+	console.log("hi");
+const titles = [];
+const hrefs = [];
+document.querySelectorAll(".title").forEach(el=> titles.push(el.outerText));
+document.querySelectorAll(".title").forEach(el=> hrefs.push(el.href.slice(el.href.indexOf('events')-1)));
+const title = titles[0];
+const href = hrefs[0];
 
 coords.forEach(function (coord, i) {
 	new mapboxgl.Marker({
@@ -43,26 +43,23 @@ map.addControl(nav, 'top-left');
 const popup = new mapboxgl.Popup({
 	closeButton: true
 });
+} 
+else {
+	//set detail page map
+	const detailtitle = document.querySelector(".detailtitle").innerText;
 
+	const marker = new mapboxgl.Marker({
+		color: 'red',
+		draggable: false
+	}).setLngLat(center)
+	.setPopup(new mapboxgl.Popup().setHTML(`<h2>${detailtitle}</h2>`))
+	.addTo(map)
 
-// const addMarker = event => {
-// 	new mapboxgl.Marker({
-// 		color: 'red',
-// 		draggable: true
-// 	})
-// 		.setLngLat(event.lngLat)
-// 		.addTo(map)
-// 		.on('dragend', event => console.log(event.target._lngLat))
-// }
+	const nav = new mapboxgl.NavigationControl();
 
-// map.on('click', addMarker)
+	map.addControl(nav, 'top-left');
 
-// popup.setLngLat(center)
-// 	.setHTML('<h1>Hello 👋</h1>')
-// 	.setMaxWidth('200px')
-// 	.addTo(map)
-
-//     new mapboxgl.Marker({
-//         color: 'red'
-//     }).setLngLat(center)
-//     .addTo(map)
+	const popup = new mapboxgl.Popup({
+		closeButton: true
+	});
+}
